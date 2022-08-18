85c6a9dc5c8e72a7fc834a0664f0a0bcd8e3d4da
Stepan Dulka <fotosteuge@gmail.com>
2022-08-18 16:25:52 +0300

    add structure to Controller

---
+++ demo/src/main/java/com/example/demo/uuid/UuidController.java
@@ -0,0 +1,15 @@
+package com.example.demo.uuid;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class UuidController {
+
+    public List<UUID> uuidList() {
+        return uuidList();
+    }
+
+}
