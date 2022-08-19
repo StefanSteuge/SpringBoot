1273746e335116b2bc9b5ae2026b7bc13665aea7
Stepan Dulka <fotosteuge@gmail.com>
2022-08-19 14:17:03 +0300

    created UUID generator

--- demo/src/main/java/com/example/demo/uuid/UuidController.java
+++ demo/src/main/java/com/example/demo/uuid/UuidController.java
@@ -1,5 +1,7 @@
 package com.example.demo.uuid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -8,8 +10,12 @@ import java.util.UUID;
 @RestController
 public class UuidController {
 
+    @Autowired
+    private UuidService uuidService;
+
+@GetMapping("/uuid")
     public List<UUID> uuidList() {
-        return uuidList();
+       return uuidService.getUuids();
     }
 
 }
---
+++ demo/src/main/java/com/example/demo/uuid/UuidService.java
@@ -0,0 +1,17 @@
+package com.example.demo.uuid;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UuidService {
+
+    public List<UUID> getUuids() {
+       return Collections.singletonList(UUID.randomUUID());
+
+    }
+
+}
