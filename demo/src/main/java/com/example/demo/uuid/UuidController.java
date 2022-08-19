e808403a4b1f79c2020a1cb5b3a95bede1f2001a
Stepan Dulka <fotosteuge@gmail.com>
2022-08-19 15:13:50 +0300

    refactor code

--- demo/src/main/java/com/example/demo/uuid/UuidController.java
+++ demo/src/main/java/com/example/demo/uuid/UuidController.java
@@ -1,21 +1,30 @@
 package com.example.demo.uuid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.UUID;
 
-@RestController
+@Controller
 public class UuidController {
 
     @Autowired
     private UuidService uuidService;
 
     @GetMapping("/uuid")
+    @ResponseBody
     public List<UUID> uuidList() {
         return uuidService.getUuids();
+    }
+
+    @GetMapping("/")
+    public String loremIpsum(Model model) {
+        model.addAttribute("title", "Lorem Ipsum");
+        return "loremIpsumPage";
     }
 
 }
